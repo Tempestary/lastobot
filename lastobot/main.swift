d5dc46d8c0c852971b022e1c66f15e3ab9a828bc
Malkavian <tempestary@gmail.com>
2020-05-11 20:15:16 +0300

    project first commit

--- lastobot/main.swift
+++ lastobot/main.swift
@@ -8,40 +8,6 @@
 
 import Foundation
 
-//apiHandler.getInfo(completion: { result in
-//    switch result {
-//    case .success (let data):
-//        print("success", data)
-//    case .failure (let error):
-//        print("error", error.localizedDescription)
-//    }
-//})
-//apiHandler.sendText(message: TextMessage(text: "превед рыс", buttons: [[Button(text: "Жми суда", callbackData: "fdgkdf;lgd")]], chatId: "752532504"),
-//        completion: { result in
-//    switch result {
-//    case .success (let data):
-//        print("success", data)
-//    case .failure (let error):
-//        print("error", error.localizedDescription)
-//    }
-//})
-
-//
-//var lastEventId = 0
-//apiHandler.getEvents(lastEventId: lastEventId, completion: { result in
-//    dump(result)
-//
-//    switch result {
-//    case .success(let events):
-//        guard let latestEvent = events.last else {
-//            return
-//        }
-//        lastEventId = latestEvent.id
-//    case .failure:
-//        return
-//    }
-//})
-
 let bot = Bot()
 bot.run()
 
